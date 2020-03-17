fcb3f2fe229cd8651560d65d00dc1315af692378
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2020-03-17 01:31:51 +0100

    add HomePage

--- app/src/main/java/com/example/webuy/MainActivity.java
+++ app/src/main/java/com/example/webuy/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener deal = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(), Main3Activity.class);
+            Intent intent = new Intent(getApplicationContext(), StartActivity.class);
             startActivity(intent);
         }
     };
