750428ab6e2993a5c3b8f71c24f5567f67746cf8
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2020-05-27 00:36:22 +0200

    [feature/Update UI] add create Purshase group activity

--- app/src/main/java/com/example/webuy/views/purshaseGroup/CreatePurshaseGroupActivity.java
+++ app/src/main/java/com/example/webuy/views/purshaseGroup/CreatePurshaseGroupActivity.java
@@ -3,6 +3,7 @@ package com.example.webuy.views.purshaseGroup;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.webuy.R;
 
@@ -12,5 +13,8 @@ public class CreatePurshaseGroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purshase_group);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.create_buying_group));
+        setSupportActionBar(toolbar);
     }
 }
