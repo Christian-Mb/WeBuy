b86a1cf515b6250f1fc1cdb25af1529a836c7e1c
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2020-05-28 23:55:06 +0200

    [feature/WebService] Add beans for Webservice

---
+++ app/src/main/java/com/example/webuy/core/pourshaseGroup/PurshaseGroup.java
@@ -0,0 +1,8 @@
+package com.example.webuy.core.pourshaseGroup;
+
+import com.example.webuy.core.utils.BaseWebuy;
+
+import java.io.Serializable;
+
+public class PurshaseGroup extends BaseWebuy implements Serializable {
+}
---
+++ app/src/main/java/com/example/webuy/core/product/Product.java
@@ -0,0 +1,43 @@
+package com.example.webuy.core.product;
+
+import com.example.webuy.core.utils.BaseWebuy;
+
+import java.io.Serializable;
+
+public class Product extends BaseWebuy implements Serializable {
+
+    private String label, content, image;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "label='" + label + '\'' +
+                ", content='" + content + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
