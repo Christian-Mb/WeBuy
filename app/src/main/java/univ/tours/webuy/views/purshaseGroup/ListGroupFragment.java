2c0deab9178471f170cdf1a7e1ef84c7fa659c8d
Christian-Mb <44323151+Christian-Mb@users.noreply.github.com>
2020-05-29 23:36:36 +0200

    [feature/Update UI] update view for GroupPurshase

--- app/src/main/java/univ/tours/webuy/views/purshaseGroup/ListGroupFragment.java
+++ app/src/main/java/univ/tours/webuy/views/purshaseGroup/ListGroupFragment.java
@@ -8,10 +8,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.webuy.R;
 
+import java.util.ArrayList;
+
+import univ.tours.webuy.core.pourshaseGroup.PurshaseGroup;
+
 public class ListGroupFragment extends Fragment {
+
+    private ArrayList<PurshaseGroup> purshaseGroups;
+
+    public void setPurshaseGroups(ArrayList<PurshaseGroup> purshaseGroups) {
+        this.purshaseGroups = purshaseGroups;
+    }
 
     @Override
     public View onCreateView(
@@ -19,13 +31,19 @@ public class ListGroupFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.magazin_fragment, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.magazinRecyclerView);
+        ListGroupAdapter listGroupAdapter = new ListGroupAdapter(getContext(), purshaseGroups);
+        recyclerView.setAdapter(listGroupAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.join_us).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(ListGroupFragment.this)
